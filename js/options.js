c0360cf6b0c972f1dae4bd6535c3e2b6fec351a4
Rob Glass <shewlayce@gmail.com>
2014-01-11 02:05:41 -0500

    Options framework finalization, Options restore correctly

--- js/options.js
+++ js/options.js
@@ -1,7 +1,8 @@
 runtimeStorage = chrome.extension.getBackgroundPage().runtimeStorage;
-queueStorage =  chrome.extension.getBackgroundPage().queueStorage; 
+queueStorage =  chrome.extension.getBackgroundPage().queueStorage;
+savedOptions = JSON.parse(localStorage['Global.Queue']);
+
 window.onload = function() {
-  queueStorage = chrome.extension.getBackgroundPage().queueStorage;
   //initOptions();
   buildQueueList();
   restoreOptions();
@@ -10,99 +11,143 @@ window.onload = function() {
   });
 }
 
-function initVariables() {
-  selectRefreshRate = document.getElementById('RefreshRate');
-  selectNotificationTimeout = document.getElementById('NotificationTimeout');
-  inputGWNUser = document.getElementById('GWNuser');
-  selectQueue = document.getElementById('Queue');
-}
-
 function buildQueueList() {
-  initVariables();
   for (i=1; i<queueStorage.length; i++) {
     var option = document.createElement('option');
     option.value = i;
     option.innerText = queueStorage[i].getName();
-    selectQueue.appendChild(option);
+    //selectQueue.appendChild(option);
   }
 }
 
 function restoreOptions() {
-  initVariables();
-  var currentRefresh = localStorage['Global.Refresh'];
-  
-  for (var i=0; i<selectRefreshRate.children.length; i++) {
-    if (selectRefreshRate[i].value == currentRefresh) {
-      selectRefreshRate[i].selected = 'true';
-      break;
-    }
+  for (var i=0;i<savedOptions.length;i++) {
+    var list = document.getElementsByClassName('placeholder')[1];
+    list.appendChild(queueList(savedOptions[i]));
   }
-  var currentNotificationTimeout = localStorage['Global.NotificationTimeout'];
-  for (var i=0; i<selectNotificationTimeout.children.length; i++) {
-    if (selectNotificationTimeout[i].value == currentNotificationTimeout) {
-      selectNotificationTimeout[i].selected = 'true';
-      break;
-    }
-  }
-  
-  if (localStorage['Global.GWNUser'] != undefined) {
-    var currentGWNuser = localStorage['Global.GWNUser'];
-    inputGWNUser.value = currentGWNuser;
-  }
-
-
 }
+
+function queueList(queue) {
+  var queueItem = document.createElement('div');
+      queueItem.className = 'queue-item';
+  var queueC= document.createElement('div');
+      queueC.className = 'queue';
+  var cover= document.createElement('div');
+      cover.className = 'cover';
+  var queueMove= document.createElement('div');
+      queueMove.className = 'queue-move';
+  var dragIcon = document.createElement('span');
+      dragIcon.className = 'icon draggable';
+  var queueTitle= document.createElement('div');
+      queueTitle.className = 'queue-title';
+  var queueTitleText = document.createElement('span');
+      queueTitleText.innerText = queueStorage[queue.queueIndex].getName();
+  var queueRemove = document.createElement('div');
+      queueRemove.className = 'queue-remove';
+  var crossIcon = document.createElement('span');
+      crossIcon.className = 'icon cross';
+
+  $(queueRemove).click(function() {
+      $(queueItem).slideToggle('fast');
+      for (var i=0;i<savedOptions.length;i++){
+        if (queue.queueIndex == savedOptions[i].queueIndex) {
+            console.log('Removing '+ i);
+            savedOptions.splice(i,1);
+            break;
+        }
+      }
+      saveOptions();
+  });
+  var queueOptions = document.createElement('div');
+      queueOptions.className = 'queueOptions';
+  var divWrapper = document.createElement('div')
+  var opt1text = document.createElement('span');
+      opt1text.innerText = 'Enable Notifications';
+  var notify = document.createElement('input');
+      notify.type = 'checkbox';
+      if (queue.notify) {
+      notify.checked = true;
+      };
+  var divWrapper2 = document.createElement('div');
+  var opt2text = document.createElement('span');
+      opt2text.innerText = 'Refresh rate:';
+  var refreshRate = document.createElement('select');
+      refreshRate.type = 'text';
+      refreshRate.id = 'RefreshRate';
+  var option1 = document.createElement('option')
+      option1.value = '60000';
+      option1.innerText = '1 minute';
+  var option2 = document.createElement('option')
+      option2.value = '300000';
+      option2.innerText = '5 minutes ';
+  var option3 = document.createElement('option')
+      option3.value = '600000';
+      option3.innerText = '10 minutes';
+  var option4 = document.createElement('option')
+      option4.value = '900000';
+      option4.innerText = '15 minutes';
+  var option5 = document.createElement('option')
+      option5.value = '1800000';
+      option5.innerText = '30 minutes';
+  var option6 = document.createElement('option')
+      option6.value = '0';
+      option6.innerText = 'Never';
+  var divWrapper3 = document.createElement('div');
+  var changeButton = document.createElement('button');
+      changeButton.innerText = 'Change';
+  
+  queueItem.appendChild(queueC);
+    queueC.appendChild(cover);
+    queueC.appendChild(queueMove);
+      queueMove.appendChild(dragIcon);
+    queueC.appendChild(queueTitle);
+      queueTitle.appendChild(queueTitleText);
+    queueC.appendChild(queueRemove);
+      queueRemove.appendChild(crossIcon);
+  queueItem.appendChild(queueOptions);
+    if (queue.queueIndex === 0) {
+      var divW = document.createElement('div');
+      var nameBox = document.createElement('input');
+          nameBox.type = "text";
+          nameBox.id = 'GWNUser';
+          nameBox.value = localStorage['Global.GWNUser'];
+      queueOptions.appendChild(divW);
+        divW.appendChild(nameBox);
+    };
+    queueOptions.appendChild(divWrapper);
+    queueOptions.appendChild(divWrapper2);
+    queueOptions.appendChild(divWrapper3);
+      divWrapper.appendChild(opt1text);
+      divWrapper.appendChild(notify)
+      divWrapper2.appendChild(opt2text);
+      divWrapper2.appendChild(refreshRate);
+        refreshRate.appendChild(option1);
+        refreshRate.appendChild(option2);
+        refreshRate.appendChild(option3);
+        refreshRate.appendChild(option4);
+        refreshRate.appendChild(option5);
+        refreshRate.appendChild(option6); 
+
+  for (var i=0;i<refreshRate.children.length;i++){
+    if (queue.updateInterval == refreshRate.children[i].value) {
+      refreshRate.children[i].selected = true;
+    break;
+    }
+  }
+  
+  // Event Listeners
+    // Slide down queueOptions  
+  $(queueTitle).click(function() {
+      $(queueOptions).slideToggle('fast');
+  });
+    // Verifies only one queue is check
+  
+  return queueItem;
+}
+
 
 function saveOptions() {
-  var newRefresh = selectRefreshRate.children[selectRefreshRate.selectedIndex].value;
-  SetOption('Global.Refresh', newRefresh);
-
-  var newNotificationTimeout = selectNotificationTimeout.children[selectNotificationTimeout.selectedIndex].value;
-  SetOption('Global.NotificationTimeout', newNotificationTimeout);
-
-  var newGWNUser = inputGWNUser.value;
-  SetOption('Global.GWNUser', newGWNUser);
-  SetOption('Global.GWNUserJQL', 'assignee = '+ newGWNUser +' and status not in (Closed, Done)');
-
-
-  UpdateIfReady(true);
+  localStorage['Global.Queue'] = JSON.stringify(savedOptions);
+  chrome.extensions.getBackgroundPage().setTheTable();
 }
-
-// Show/hide suboptions at startup)
-//function initOptions() {
-//  var a=1;
-//  $("input[type='checkbox'], input[type='radio']").each(function(index) {
-//    var $fieldset = $(this).parent("legend").parent("fieldset");
-//    var $options = $fieldset.children(".subOptions").first();
-//    
-//    $fieldset.toggleClass("selected", this.checked);        
-//    $options.toggle(this.checked);
-//
-//    // Bind function to show/hide options
-//    if ($(this).attr("type") == "checkbox") {
-//      $(this).change(function() {
-//        $fieldset.toggleClass("selected", this.checked);
-//        if (this.checked) {
-//          $options.slideDown();
-//        } else {
-//          $options.slideUp();                   
-//        }
-//      });
-//    } else if ($(this).attr("type") == "radio") {
-//      $(this).change(function() {
-//        var inputName = $(this).attr("name");
-//        $("input[name='" + inputName + "']").each(function(index, input) {
-//          $fieldset = $(this).parent("legend").parent("fieldset");
-//          $fieldset.toggleClass("selected", this.checked);
-//          var $subOptions = $fieldset.children(".subOptions").first();
-//          if (this.checked) {
-//            $subOptions.slideDown();
-//          } else {
-//            $subOptions.slideUp();  
-//          }
-//        });
-//      });
-//    }
-//  });
-//}
 
